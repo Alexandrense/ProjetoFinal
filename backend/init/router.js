66cbac2bcb62f1071be6ad78548b89832e136fca
Alexandre Carvalho <alexbh@gmail.com>
2023-01-28 22:33:00 +0000

    remove backend legacy

--- backend/init/router.js
+++ backend/init/router.js
@@ -5,7 +5,6 @@ module.exports = (app) => {
     app.use('/auth', require('../routes/auth.routes'));
     app.use('/users/levels', require('../routes/userlevel.routes'));
     app.use('/users', require('../routes/user.routes'));
-    app.use('/emails', require('../routes/email.routes'));
     app.use('/dailyregistries', require('../routes/dayRegistry.routes'));
     app.use('/patients', require('../routes/patient.routes'));
 }
